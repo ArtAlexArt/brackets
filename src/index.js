068acb9b4c2292ffa5e9cb9126f8c06bad28d909
Alexander Artyomchenko <56248476+ArtAlexArt@users.noreply.github.com>
2022-02-24 22:10:41 +0200

    feat: add source

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const start = [],
+    rr = {};
+  let stack = [];
+  bracketsConfig.forEach((item) => {
+    start.push(item[0]);
+    rr[item[1]] = item[0];
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    let curr = str[i];
+    if (stack[stack.length - 1] === curr && rr[curr] === curr) {
+      stack.pop();
+    } else if (start.includes(curr)) {
+      stack.push(curr);
+    } else {
+      if (stack.length === 0) return false;
+      if (rr[curr] === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return !stack.length;
+};
